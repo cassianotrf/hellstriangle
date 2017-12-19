870f4d23c1ad3bf378348dc5ccfe9c60e8ff1613
cassianotrf <32061826+cassianotrf@users.noreply.github.com>
2017-12-19 14:43:59 -0200

    Add files via upload

--- hellstriangle.rb
+++ hellstriangle.rb
@@ -1,26 +1,41 @@
-b = [[6],[3,5],[7,9,1],[4,6,8,4]]
+a = [[6],[3,5],[7,9,1],[4,6,8,4]]
 
 def multi_array_sum(a)
   sum = 0
   i = 0
   j = 0
   sum = a[i][j]
+  #sum is getting the first value from the array a[0][0] = 6
   tam_i = a.length - 1
 
   while tam_i > 0
-
+    #example: checking if the value "3" from the position [1][0] is bigger than
+    #the value "5" from the position [1][1]
     if a[i + 1][j] > a[i + 1][j +1]
+      #if yes we add the value 3 into sum
       sum = sum + a[i + 1][j]
     else
+       #else we add the value 5 into sum
      sum = sum + a[i + 1][j +1]
      j += 1
    end
-
    tam_i -= 1
    i += 1
   end
   puts sum
 end
 
-multi_array_sum(b)
+multi_array_sum(a)
 
+#If you wanna test, unmark the # till line 48
+# def validate_multi_array_sum(a,answer)
+#   if multi_array_sum(a) == answer
+#     puts "Test done without errors"
+#   else
+#     puts "Function has errors, the answer should be #{answer} and your result was"
+#     multi_array_sum(a)
+#   end
+# end
+
+#answer = 26
+# #validate_multi_array_sum(a,answer)
